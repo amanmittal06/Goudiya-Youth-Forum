3011023a00835d2e5c8c390aed32a6a4856acc21
amanmittal06 <129789134+amanmittal06@users.noreply.github.com>
2024-07-06 00:40:38 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { RouterProvider,  createBrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, RouterProvider,  createBrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 import Newsletter from './Components/NewsLetter/NewsLetter.jsx'
@@ -9,18 +9,32 @@ import About from './Components/About us/About.jsx'
 import SignUp from './Components/Sign up/SignUp.jsx'
 import Store from './Components/Store/Store.jsx'
 
-const router = createBrowserRouter([
-  {path:"/", element: <App/>, children: [
-    {path: "/", element:<Home></Home>},
-    {path:"/newsletter", element: <Newsletter/>},
-    {path:"/store" , element: <Store></Store>},
-    {path:"/about-us", element:<About></About>}
-  ]},
-  {path:"/sign-up", element: <SignUp></SignUp>}
-])
+// const router = createBrowserRouter({
+//   basename: "/Goudiya-Youth-Forum/",
+//   routes: [
+//     {path:"/", element: <App/>, children: [
+//       {path: "/", element:<Home></Home>},
+//       {path:"/newsletter", element: <Newsletter/>},
+//       {path:"/store" , element: <Store></Store>},
+//       {path:"/about-us", element:<About></About>}
+//     ]},
+//     {path:"/sign-up", element: <SignUp></SignUp>}
+//   ]
+// })
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router}></RouterProvider>
+    {/* <RouterProvider router={router.routes}><App></App></RouterProvider> */}
+    <BrowserRouter basename="/Goudiya-Youth-Forum/"> {/* Replace with your base URL */}
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<Home />} />
+          <Route path="newsletter" element={<Newsletter />} />
+          <Route path="store" element={<Store />} />
+          <Route path="about-us" element={<About />} />
+        </Route>
+        <Route path="sign-up" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
