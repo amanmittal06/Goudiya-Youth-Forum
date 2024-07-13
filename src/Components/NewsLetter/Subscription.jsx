98b53dea629556ccccadd309a9642626f16314f7
amanmittal06 <amanagrawal061202@gmail.comm>
2024-07-13 22:01:50 +0530

    Updated archives

--- src/Components/NewsLetter/Subscription.jsx
+++ src/Components/NewsLetter/Subscription.jsx
@@ -3,13 +3,17 @@ import styles from './Subscription.module.css'
 
 const Subscription =()=>{
 
+    function handleSubmit(event){
+        event.preventDefault();
+    }
+
     return(
 
         <div className={styles.subscription}>
           <p className={styles.text}>Subscribe now to ensure you receive all the enriching content straight to your inbox and stay connected with the Goudiya Youth Forum!</p>
           <form className={styles.subscriptionForm} action="#">
             <input className={styles.emailInput} type="email" name="email" id="email" placeholder="Enter your email"/>
-            <button className={styles.subscriptionButton}>Subscribe</button>
+            <button className={styles.subscriptionButton} onClick={(event)=>{handleSubmit(event)}}>Subscribe</button>
           </form>
         </div>
     )
