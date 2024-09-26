34aff6dda21cb0bb0653c5a60585e86109664ef9
amanmittal06 <amanagrawal061202@gmail.comm>
2024-09-26 09:30:14 +0530

    updated

--- src/Components/Store/TopBar.jsx
+++ src/Components/Store/TopBar.jsx
@@ -4,13 +4,14 @@ import gyfLogoPNG from './Images/gyfLogoPNG.png'
 import { MdOutlineMenu } from "react-icons/md";
 import { RxCross1 } from "react-icons/rx";
 import { useState , useEffect} from 'react';
-import { GoogleLogin, googleLogout } from '@react-oauth/google';
-import { jwtDecode } from 'jwt-decode';
+import { useAuth0 } from '@auth0/auth0-react';
+
 
 
 const TopBar = ({title}) => {
 
     const [menuActive, setMenuActive] = useState(false);
+    const {loginWithRedirect, logout, isAuthenticated }  =useAuth0();
     const toggleMenu = ()=>{
         setMenuActive(!menuActive);
         scrollTo({
@@ -19,44 +20,11 @@ const TopBar = ({title}) => {
         
     }
 
-    const [user, setUser] = useState(() => {
-        // Initialize state from local storage
-        const savedUser = localStorage.getItem('user');
-        return savedUser ? JSON.parse(savedUser) : null;
-    });
-    const [isAuthenticated, setIsAuthenticated] = useState(!!user);
-    
-    useEffect(() => {
-        // Save user data to local storage when it changes
-        if (user) {
-          localStorage.setItem('user', JSON.stringify(user));
-        } else {
-          localStorage.removeItem('user');
-        }
-    }, [user]);
+ 
 
-    // Handle the successful login
-    const handleLoginSuccess = (response) => {
-        const decoded = jwtDecode(response.credential);  // Decode JWT token to get user info
-        console.log(decoded);  // Log the user data to see what info you get
-        setUser({
-            name: decoded.name,
-            email: decoded.email,
-        });
-        setIsAuthenticated(true); 
-    };
-
-    // Handle login failure
-    const handleLoginFailure = () => {
-        console.log('Login Failed');
-    };
-
-    // Handle logout
-    const handleLogout = () => {
-        googleLogout();  // Logs out the user from Google
-        setUser(null);  // Clear user data
-        setIsAuthenticated(false);  // Mark the user as not authenticated
-    };
+ 
+   
+   
 
     return(
         <div>
@@ -71,17 +39,13 @@ const TopBar = ({title}) => {
               {isAuthenticated===true?
               <div className={styles.menuContainer}>
                   <button className={styles.buttons}>Orders</button>
-                  <button className={styles.buttons} onClick={()=>{handleLogout()}}>Log out</button>
+                  <button className={styles.buttons}  onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>Log out</button>
               </div>
               :
               <div className={styles.loginContainer}>
                     <div>
                     <div style={{marginBottom:'5%'}}>Kindly log in to view your orders!</div>
-                    <GoogleLogin
-                        onSuccess={handleLoginSuccess}
-                        onError={handleLoginFailure}
-                        className= {styles.loginButton}
-                    />
+                    <button onClick={() => loginWithRedirect()}>Log in</button>
                     </div>
               </div>}
            </div>
--- src/main.jsx
+++ src/main.jsx
@@ -10,13 +10,19 @@ import Store from './Components/Store/Store.jsx'
 import AllVolumes from './Components/NewsLetter/AllVolumes.jsx'
 import ScrollToTop from './Components/ScrollToTop.jsx'
 import RegistrationForm from './Components/Home/RegistrationForm.jsx'
-import { GoogleOAuthProvider } from '@react-oauth/google';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-  <GoogleOAuthProvider  clientId="599101796726-liinvmodq1fnf13nbepju9134vvjmeur.apps.googleusercontent.com"> 
+  <Auth0Provider
+    domain="dev-y6rwbvo0s0fei8p3.us.auth0.com"
+    clientId="hYomYOZK9E8UmTTf05iejxCpQmY88EhR"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  > 
     <BrowserRouter basename=''> 
       <ScrollToTop/>
       <Routes>
@@ -30,6 +36,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <Route path='/register' element={<RegistrationForm />} />
       </Routes>
     </BrowserRouter>
-  </GoogleOAuthProvider>  
+  </Auth0Provider>  
   </React.StrictMode>,
 )
