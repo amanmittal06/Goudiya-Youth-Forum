0bb7d00fa3f0a2a3db94b2edd606dfd02ac98cee
amanmittal06 <amanagrawal061202@gmail.comm>
2024-09-29 07:13:18 +0530

    Updated

--- src/Components/Store/MyOrders.jsx
+++ src/Components/Store/MyOrders.jsx
@@ -1,24 +1,19 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import axios from "axios";
 import { useEffect, useState } from "react"
+import styles from './MyOrders.module.css'
 
 
 const MyOrders = ()=>{
 
     const [orders, setOrders] = useState([]);
     const {isAuthenticated, user} = useAuth0();
-    const {sizes, setSizes} = useState([]);
+   
 
     const loadOrders = async() =>{
      try{
         const res = axios.get(`https://gyf-backend.vercel.app/orders/:${user.email}`)
         setOrders(res);
-
-        orders.map((order)=>{
-            if(order.items.sizes){
-                setSizes(order.items.sizes);
-            }
-        })
      }
      catch(err){
         console.log(err);
@@ -33,53 +28,57 @@ const MyOrders = ()=>{
     // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [isAuthenticated, user]);
 
+  
     return(
         isAuthenticated && 
-        <div>
-            <div>
+        <center className={styles.container}>
+            <div className={styles.topHeading}>
+                Your Orders
             </div>
             {
                orders.length===0?
                <div>No orders have been yet</div>
                :
                orders.map((order)=>(
-                  <div key={order._id}>
-                     <div>Order id: {order._id}</div>
+                  <div key={order._id} className={styles.order}>
+                     <div style={{textAlign:'start', fontWeight:500}}>Order id: <span style={{fontSize:'small'}}>{order._id}</span></div>
                      <div>
                         {
                             order.items.map((item)=>(
                                item.sizes?
-                               sizes.map((size)=>(
-                                <div key={size._id}> 
-                                  <div>{item.title} {size.type} </div> <p>×</p> <div>{size.quantity}</div>
+                               item.sizes.map((size)=>(
+                                <div key={size._id} className={styles.item}> 
+                                  <div style={{width:'60%', textAlign:'left'}}>{item.title} {size.type} </div> <p style={{width:'10%'}}>×</p> <div style={{width:'30%', textAlign:'right'}}>{size.quantity}</div>
                                 </div>
                                ))
                                :
-                               <div key={item._id}> 
+                               <div key={item._id} className={styles.item}> 
                                   <div>{item.title}</div> <p>×</p> <div>{item.quantity}</div>
                                </div>
                             ))
                         }
-                        <div>
-                            <div>
-                              <span style={{fontFamily:'sans-serif'}}>₹</span>{order.amount}
-                            </div>
+                        <div className={styles.priceAndStatus}>
+                            
+
                             {
                                 order.delivered===true?
-                                <div>
-                                    delivered
+                                <div style={{fontWeight:'400'}}>
+                                   Status: Delivered
                                 </div>
                                 :
-                                <div>
-                                    not delivered
+                                <div style={{fontWeight:'400'}}>
+                                   Status: Not delivered
                                 </div>
                             }
+                             <div style={{fontWeight:'400'}}>
+                              <span style={{fontFamily:'sans-serif'}}>₹</span>{order.amount}
+                            </div>
                         </div>
                      </div>
                   </div>
                )) 
             }
-        </div>
+        </center>
     )
 
 
