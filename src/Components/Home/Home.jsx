e58d8208a25ca84f29da02baf8a329f016e65fea
amanmittal06 <amanagrawal061202@gmail.comm>
2024-09-30 16:28:53 +0530

    .

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -5,24 +5,9 @@ import FooterSection from "./FooterSection"
 import RecentActivities from "./RecentActivities"
 import styles from './Home.module.css'
 import { IoMdArrowDropup } from "react-icons/io";
-import Carousel from "./Carousel"
 
-import Image1 from './Images/Home1.jpg'
-import Image2 from './Images/Home2.jpg'
-import Image3 from './Images/Home3.jpg'
-import Image4 from './Images/Jhinjholi.jpg'
-import Image5 from './Images/Jhinjholi2.jpg'
-import Image6 from './Images/Jhinjholi3.jpg'
 
-const slides = [
-  { image: Image1, title: 'Slide 1' },
-  { image: Image2, title: 'Slide 2' },
-  { image: Image3, title: 'Slide 3' },
-  { image: Image4, title: 'Slide 4' },
-  { image: Image5, title: 'Slide 5' },
-  { image: Image6, title: 'Slide 6' },
-  // Add more slides as needed
-];
+
 
 const Home  = () =>{
 
@@ -38,7 +23,6 @@ const Home  = () =>{
     <>
       <Header></Header>
       <WelcomeMsgAndIntro></WelcomeMsgAndIntro>
-      <div style= {{margin:'5%'}}><Carousel slides={slides}></Carousel></div>
       <RecentActivities></RecentActivities>
       <ChaptersSection></ChaptersSection>
       <center><button className={styles.backToTop} onClick={handleOnClick}>Back To Top<IoMdArrowDropup/></button></center>
--- src/Components/Home/WelcomeMsgAndIntro.jsx
+++ src/Components/Home/WelcomeMsgAndIntro.jsx
@@ -3,12 +3,28 @@
 import styles from './WelcomeMsgAndIntro.module.css'
 import { useNavigate } from 'react-router-dom'
 import { RxCross2 } from "react-icons/rx";
-
+import Image1 from './Images/Home1.jpg'
+import Image2 from './Images/Home2.jpg'
+import Image3 from './Images/Home3.jpg'
+import Image4 from './Images/Jhinjholi.jpg'
+import Image5 from './Images/Jhinjholi2.jpg'
+import Image6 from './Images/Jhinjholi3.jpg'
+import Carousel from './Carousel';
 
 
 
 
 const WelcomeMsgAndIntro=()=>{
+
+    const slides = [
+        { image: Image1, title: 'Slide 1' },
+        { image: Image2, title: 'Slide 2' },
+        { image: Image3, title: 'Slide 3' },
+        { image: Image4, title: 'Slide 4' },
+        { image: Image5, title: 'Slide 5' },
+        { image: Image6, title: 'Slide 6' },
+        // Add more slides as needed
+      ];
     
     function handleCancelButton(){
         const element = document.getElementsByClassName(styles.welcomeMessage)
@@ -38,6 +54,7 @@ const WelcomeMsgAndIntro=()=>{
                  {/* <Link to='/register' className={styles.registerButton}>Register</Link> */}
                </div>
                {/* <RecentActivities></RecentActivities> */}
+               <div style= {{margin:'5%'}}><Carousel slides={slides}></Carousel></div>
                <div className={styles.aboutHeading}>About Goudiya Youth Forum</div>
                <div className={styles.aboutDescription}>At the Goudiya Youth Forum, we are committed to nurturing a community where young individuals delve into the profound teachings of scriptures such as Shrimad Bhagavad Gita. Our programs include <strong>regular classes</strong> that explore scripture deeply, transformative <strong>workshops</strong>, and immersive <strong>spiritual study tours</strong> that provide hands-on learning experiences. These initiatives are designed to inspire personal growth, spiritual enlightenment, and the practical application of timeless wisdom.  
                </div>
