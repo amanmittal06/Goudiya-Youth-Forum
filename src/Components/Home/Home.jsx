d7cd4e93e0e872be3412849c93a3192170ecfb2b
amanmittal06 <amanagrawal061202@gmail.comm>
2024-09-30 16:21:12 +0530

    .

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -7,14 +7,20 @@ import styles from './Home.module.css'
 import { IoMdArrowDropup } from "react-icons/io";
 import Carousel from "./Carousel"
 
-import Image from './Images/Jhinjholi.jpg'
-import Image2 from './Images/Jhinjholi2.jpg'
-import Image3 from './Images/Jhinjholi3.jpg'
+import Image1 from './Images/Home1.jpg'
+import Image2 from './Images/Home2.jpg'
+import Image3 from './Images/Home3.jpg'
+import Image4 from './Images/Jhinjholi.jpg'
+import Image5 from './Images/Jhinjholi2.jpg'
+import Image6 from './Images/Jhinjholi3.jpg'
 
 const slides = [
-  { image: Image, title: 'Slide 1' },
+  { image: Image1, title: 'Slide 1' },
   { image: Image2, title: 'Slide 2' },
-  { image: Image3, title: 'Slide 3' }
+  { image: Image3, title: 'Slide 3' },
+  { image: Image4, title: 'Slide 4' },
+  { image: Image5, title: 'Slide 5' },
+  { image: Image6, title: 'Slide 6' },
   // Add more slides as needed
 ];
 
