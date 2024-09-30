ffb9fdea17eca8fbe4916db9ad3d7beba94872f1
amanmittal06 <amanagrawal061202@gmail.comm>
2024-09-30 15:22:09 +0530

    updated

--- src/Components/Home/WelcomeMsgAndIntro.jsx
+++ src/Components/Home/WelcomeMsgAndIntro.jsx
@@ -1,11 +1,8 @@
 /* eslint-disable react/no-unescaped-entities */
 
 import styles from './WelcomeMsgAndIntro.module.css'
-import gyfLogoPNG from './Images/gyfLogoPNG.png'
-import GouiyaYouthForum from './Images/GoudiyaYouthForum.png'
-import { Link, useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import { RxCross2 } from "react-icons/rx";
-import RecentActivities from './RecentActivities';
 
 
 
@@ -42,7 +39,7 @@ const WelcomeMsgAndIntro=()=>{
                </div>
                {/* <RecentActivities></RecentActivities> */}
                <div className={styles.aboutHeading}>About Goudiya Youth Forum</div>
-               <div className={styles.aboutDescription}>At the Goudiya Youth Forum, we are committed to nurturing a community where young individuals delve into the profound teachings of scriptures such as the Bhagavad Gita. Our programs include <strong>regular classes</strong> that explore scripture deeply, transformative <strong>workshops</strong>, and immersive <strong>spiritual study tours</strong> that provide hands-on learning experiences. These initiatives are designed to inspire personal growth, spiritual enlightenment, and the practical application of timeless wisdom.  
+               <div className={styles.aboutDescription}>At the Goudiya Youth Forum, we are committed to nurturing a community where young individuals delve into the profound teachings of scriptures such as Shrimad Bhagavad Gita. Our programs include <strong>regular classes</strong> that explore scripture deeply, transformative <strong>workshops</strong>, and immersive <strong>spiritual study tours</strong> that provide hands-on learning experiences. These initiatives are designed to inspire personal growth, spiritual enlightenment, and the practical application of timeless wisdom.  
                </div>
 
         </div>
--- src/Components/NewsLetter/EditorialCommittee.jsx
+++ src/Components/NewsLetter/EditorialCommittee.jsx
@@ -1,14 +1,13 @@
 import styles from './EditorialCommittee.module.css'
-import Human from './Images/Human.png'
 import NikhilGrewal from './Images/NikhilGrewal.jpg'
 import GehnaSahni from './Images/GehnaSahni.jpg'
 import ShreyaGrewal from './Images/ShreyaGrewal.jpg'
 import IntroCard from './IntroCard';
 
 const committeeMembers = [
-    {id:1 , nAme : 'Nikhil Grewal', image: NikhilGrewal , designation: 'Editorial Designer' , description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Error quo sunt dolorum, distinctio libero dignissimos perspiciatis doloribus accusamus nobis fugit esse corrupti aliquid repellat tempore possimus aliquam consectetur optio debitis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Error quo sunt dolorum, distinctio libero dignissimos perspiciatis doloribus accusamus nobis fugit esse corrupti aliquid repellat tempore possimus aliquam consectetur optio debitis.' },
-    {id:2 , nAme : 'Gehna Sahni', image: GehnaSahni , designation: 'Contributing writer' , description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Error quo sunt dolorum, distinctio libero dignissimos perspiciatis doloribus accusamus nobis fugit esse corrupti aliquid repellat tempore possimus aliquam consectetur optio debitis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Error quo sunt dolorum, distinctio libero dignissimos perspiciatis doloribus accusamus nobis fugit esse corrupti aliquid repellat tempore possimus aliquam consectetur optio debitis.' },
-    {id:3 , nAme : 'Shreya Grewal', image: ShreyaGrewal , designation: 'Proofreader' , description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Error quo sunt dolorum, distinctio libero dignissimos perspiciatis doloribus accusamus nobis fugit esse corrupti aliquid repellat tempore possimus aliquam consectetur optio debitis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Error quo sunt dolorum, distinctio libero dignissimos perspiciatis doloribus accusamus nobis fugit esse corrupti aliquid repellat tempore possimus aliquam consectetur optio debitis.' }
+    {id:1 , nAme : 'Nikhil Grewal', image: NikhilGrewal , designation: 'Editorial Designer' , description: 'Shri Nikhil Grewal holds B.Tech in Computer Science and Engineering from Netaji Shubhas University of Technology(formerly NSIT, Delhi). He became member of GYF in December 2022 and has been taking very serious interest in all activities of GYF. Since the inception of GYF Newsletter he has been designing the entire letter single handedly as core member of GYF Newsletter committee.' },
+    {id:2 , nAme : 'Gehna Sahni', image: GehnaSahni , designation: 'Contributing writer' , description: 'Ms. Gehna Sahni holds a B.A in socialogy from Bharti college, University of Delhi. She holds a masters in Developemental Studies from Ambedkar University. She is a founding member of GYF and a prolific writer. She edits the content of the newsletter and additionally writes her own articles.' },
+    {id:3 , nAme : 'Shreya Grewal', image: ShreyaGrewal , designation: 'Proofreader' , description: 'Ms. Shreya Grewal holds B.Sc. in Mathematics from Bharti College, University of Delhi. She joined GYF in March 2023 and has been taking very serious interest in GYF activities. She is known for her precision and focus. She helps in layout and design of the newsltter in addition does a entire proof reading of the entire newsletter.' }
   ];
 
 const EditorialCommittee = () => {
