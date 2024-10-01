532e411293d820f8ca123f433b3044c658e85928
amanmittal06 <amanagrawal061202@gmail.comm>
2024-10-02 01:28:44 +0530

    .

--- src/Components/Store/MyOrders.jsx
+++ src/Components/Store/MyOrders.jsx
@@ -108,7 +108,7 @@ const MyOrders = ()=>{
                                     <button onClick={()=>changePaymentStatus(order._id, 'Under verification')} className={styles.submitButton}>Submit</button>
                                  </div>
                                  :
-                                 <div>{order.paymentId} <button onClick={()=>openInputBox(order._id)} className={styles.editButton}>Edit</button></div>) 
+                                 <div>{order.paymentId} <p onClick={()=>openInputBox(order._id)} className={styles.editButton}>Edit</p></div>) 
                               :
                               (
                                  order.paymentStatus=='Due'?
