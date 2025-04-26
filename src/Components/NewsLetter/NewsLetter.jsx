21580821c3e9e517101e63867ae4c3cb98901365
amanmittal06 <amanagrawal0139@gmail.com>
2025-04-26 07:49:36 +0530

    Harded coded latest newsletter

--- src/Components/NewsLetter/NewsLetter.jsx
+++ src/Components/NewsLetter/NewsLetter.jsx
@@ -11,34 +11,39 @@ import { useEffect, useState } from "react"
 
 const Newsletter = () =>{
 
-    let [latestNewsletter, setLatestNewsletter] = useState([]);
-    let [response, setResponse] = useState([]);
+    // let [latestNewsletter, setLatestNewsletter] = useState([]);
+    // let [response, setResponse] = useState([]);
 
-    const fetchNewsletter = async() =>{
-     try {
-        const res = await axios.get(`https://gyf-backend.vercel.app/newsletters/latest`);
-        setResponse(res.data[0]);
+    // const fetchNewsletter = async() =>{
+    //  try {
+    //     const res = await axios.get(`https://gyf-backend.vercel.app/newsletters/latest`);
+    //     setResponse(res.data[0]);
 
-     } catch (error) {
-        console.error('Error while fetching user details:', error);
-     }
-    }
+    //  } catch (error) {
+    //     console.error('Error while fetching user details:', error);
+    //  }
+    // }
 
-    useEffect(()=>{
-      fetchNewsletter();
-    }, [])
+    // useEffect(()=>{
+    //   fetchNewsletter();
+    // }, [])
 
-    useEffect(()=>{
-      setLatestNewsletter([
-        {id:response._id, title:`GYF NEWSLETTER VOLUME-${response.volume}`, description: response.description, coverPage: response.coverImage, downloadLink: response.address}
-      ])
-    }, [response])
+    // useEffect(()=>{
+    //   setLatestNewsletter([
+    //     {id:response._id, title:`GYF NEWSLETTER VOLUME-${response.volume}`, description: response.description, coverPage: response.coverImage, downloadLink: response.address}
+    //   ])
+    // }, [response])
+
+    const latestNewsletter = [
+      {id:'1', title:'GYF NEWSLETTER VOLUME-16', description: undefined, coverPage: "/newsletters/CoverImages/CoverImageVol16.png", downloadLink: "/newsletters/volume16.pdf"}
+    ];
 
    
     return(
         <>
           <TopBar title={`MONTHLY NEWSLETTER`}></TopBar>
-          {response.volume!=undefined && <div className={styles.latest}><LatestRealease latestNewsletter={latestNewsletter}></LatestRealease></div>}
+          <div className={styles.latest}><LatestRealease latestNewsletter={latestNewsletter}></LatestRealease></div>
+          {/* {response.volume!=undefined && <div className={styles.latest}><LatestRealease latestNewsletter={latestNewsletter}></LatestRealease></div>} */}
           <VoulmesAndSubscription></VoulmesAndSubscription>
           <EditorialCommittee></EditorialCommittee>
         </>
